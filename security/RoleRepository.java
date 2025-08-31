2abacf2af166554d1b1f0d1ce2e36b691815a3d3
MohamedElsedawy21 <mohamedtamerelsedawy@gmail.com>
2025-08-31 13:00:27 +0300

    Delete security directory

--- security/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.university.university.security;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
